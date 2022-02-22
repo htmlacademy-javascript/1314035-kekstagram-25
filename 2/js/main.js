a114ca5fb79191436353e7c32e25536b7fdfeeea
Keks <keks@htmlacademy.ru>
2022-02-22 14:13:17 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/main.js
+++ 2/js/main.js
@@ -1,18 +1,14 @@
 // Источник: hhttps://www.schoolsw3.com/js/js_random.php
 
 function getRandomNumber(min, max) {
-  const random = Math.floor(Math.random() * (max - min + 1)) + min;
   if (max === min || max < 0 || min < 0 )  {
     return ('Значения не могут быть меньше нуля или равны');
   }
-  return random;
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 getRandomNumber();
 
 function checkLengthString(string, max) {
-  if (string.length <= max) {
-    return true;
-  }
-  return false;
+  return (string.length <= max);
 }
 checkLengthString();
