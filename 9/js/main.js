4ba22fdcb3a96a7d2c0fa9a9a1f66a2380aa529f
Keks <keks@htmlacademy.ru>
2022-04-13 23:21:20 +0300

    :heavy_check_mark: Сборка #9

--- 9/js/main.js
+++ 9/js/main.js
@@ -1,4 +1,4 @@
-import {similarItems} from './data.js';
+// import {similarItems} from './data.js';
 import {makeThumbnails} from './thumbnail.js';
 import './user-modal.js';
 import './scale-control.js';
@@ -7,10 +7,16 @@ import {closeUserModal} from './user-modal.js';
 import {setUserFormSubmit} from './user-form.js';
 import {getData} from './load.js';
 import {setButtonsContainerClick} from './filters.js';
+import {debounce} from './util.js';
+import {onThumbnailsClick} from './big-picture.js';
 
-getData(() => {
-  makeThumbnails();
-  setButtonsContainerClick(() => makeThumbnails());
+const RERENDER_DELAY = 500;
+
+getData((similarItems) => {
+  makeThumbnails(similarItems);
+  setButtonsContainerClick(debounce(
+    (sortedData) => makeThumbnails(sortedData),
+    RERENDER_DELAY,
+  ));
 });
-
 setUserFormSubmit(closeUserModal);
